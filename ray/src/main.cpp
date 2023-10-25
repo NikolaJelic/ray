4dc3e682c922abcab23b8353357d225bf6f9556e
NikolaJelic <jelicnikola37@gmail.com>
2023-10-25 20:44:23 +0200

    add simple ppm image generator function

---
+++ ray/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <ray/render/image_render.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    ray::ImageRender imageRender{1000, 1000};
+    std::ofstream out("out.ppm", std::ios::out);
+    imageRender.render_image(out);
+    return 0;
+}
